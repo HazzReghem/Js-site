de4f5fac8b0b89cd8d0cb3aada1df70728870fff
Thibault Roussel <thibault.roussel@gmx.com>
2022-12-13 12:05:33 +0100

    début cart.js - Local Storage fonctionne pas c'est terrible

---
+++ front/js/cart.js
@@ -0,0 +1,39 @@
+let customerCart = JSON.parse(localStorage.getItem("product"));
+console.log(customerCart);
+
+// Fonction d'affichage des produits dans le panier
+
+function displayProductsInCart() {
+    
+    // Si le panier est vide : Erreur
+    if(customerCart === null) {
+        alert("Il n'y a aucun article dans votre panier !")
+    }else {
+        for (i = 0; i < customerCart.length; i += 1) {
+            document.querySelector("#cart__items").innerHTML += 
+            `<article class="cart__item" data-id="${customerCart[i].id}" data-color="${customerCart[i].color}">
+                <div class="cart__item__img">
+                  <img src="${customerCart[i].image}" alt="${customerCart[i].alt}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${customerCart[i].name}</h2>
+                    <p>${customerCart[i].color}</p>
+                    <p>${customerCart[i].price}</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : ${customerCart[i].quantity} </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>`
+        }
+    }
+}
+
+displayProductsInCart()
