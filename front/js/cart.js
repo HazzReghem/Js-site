a22dee94a1f8999020d9f29eac394b16b05a4f4d
Thibault Roussel <thibault.roussel@gmx.com>
2022-12-19 12:45:20 +0100

    fonction display Prix total

--- front/js/cart.js
+++ front/js/cart.js
@@ -12,10 +12,10 @@ function displayProductsInCart() {
     }else {
         for (i = 0; i < customerCart.length; i++ ) {
 
-          const colorChoice = customerCart[i].color;
+          let colorChoice = customerCart[i].color;
           console.log(colorChoice)
 
-          const quantityChoice = customerCart[i].quantity;
+          let quantityChoice = customerCart[i].quantity;
           console.log(quantityChoice)
 
           fetch(`http://localhost:3000/api/products/${customerCart[i].id}`)
@@ -32,7 +32,7 @@ function displayProductsInCart() {
                     <div class="cart__item__content__description">
                       <h2>${data.name}</h2>
                       <p>${colorChoice}</p>
-                      <p>${data.price}€</p>
+                      <p>${quantityChoice * data.price}€</p>
                     </div>
                     <div class="cart__item__content__settings">
                       <div class="cart__item__content__settings__quantity">
@@ -45,36 +45,41 @@ function displayProductsInCart() {
                     </div>
                   </div>
                 </article>`
+
+              let quantityNumber = Number(quantityChoice);
+              console.log(quantityNumber)
+    
+              let priceNumber = Number(quantityChoice * data.price);
+              console.log(priceNumber)
+
+              // totalQuantity.push(quantityNumber);
+              // totalPrice.push(priceNumber);
+
+              // const total = (accumulator, currentValue) => accumulator + currentValue;
+              // const totalPrice = priceNumber.reduce(total);
             })
             
             .catch(error => {
               alert("Oops, une erreur s'est produite ! Veuillez recharger la page.")
             })
-
-            // document.querySelector("#cart__items").innerHTML += 
-            // `<article class="cart__item" data-id="${customerCart[i].id}" data-color="${customerCart[i].color}">
-            //     <div class="cart__item__img">
-            //       <img src="${customerCart[i].image}" alt="${customerCart[i].alt}">
-            //     </div>
-            //     <div class="cart__item__content">
-            //       <div class="cart__item__content__description">
-            //         <h2>${customerCart[i].name}</h2>
-            //         <p>${customerCart[i].color}</p>
-            //         <p>${customerCart[i].price}€</p>
-            //       </div>
-            //       <div class="cart__item__content__settings">
-            //         <div class="cart__item__content__settings__quantity">
-            //           <p>Qté : </p>
-            //           <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${customerCart[i].quantity}">
-            //         </div>
-            //         <div class="cart__item__content__settings__delete">
-            //           <p class="deleteItem">Supprimer</p>
-            //         </div>
-            //       </div>
-            //     </div>
-            //   </article>`
         }
     }
 }
-
 displayProductsInCart();
+
+// function displayTotals () {
+//     const total = (accumulator, currentValue) => accumulator + currentValue;
+
+//       // Additionner les quantités totales de chaque produit du panier
+//           const realTotalQuantity = totalQuantity.reduce(total, 0);
+//           console.log(realTotalQuantity)
+
+//       // Additionner les prix totaux de chaque produit du panier      
+//           const realTotalPrice = totalPrice.reduce(total, 0);
+//           console.log(realTotalPrice)
+
+//           document.querySelector('.cart__price').innerHTML +=
+//           `<p>Total (<span id="totalQuantity"><!-- 2 --></span> articles) : <span id="totalPrice">${totalPrice}</span>`
+// }
+
+// displayTotals();
